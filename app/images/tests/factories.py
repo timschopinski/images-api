192a50191e0abace75f55ba5b7f4d3df050630a9
Tim Schopinski <91907758+timschopinski@users.noreply.github.com>
2023-09-22 12:34:25 +0200

    fix tests

--- app/images/tests/factories.py
+++ app/images/tests/factories.py
@@ -17,16 +17,18 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    username = factory.Faker('user_name')
+    username = factory.Sequence(lambda n: f'user{n}')
     email = factory.Faker('email')
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
-    password = factory.PostGenerationMethodCall('set_password', 'password')
+    password = factory.PostGenerationMethodCall('set_password', 'password123#')
 
     @factory.post_generation
     def create_user_profile(self, created, extracted, **kwargs):
         if created:
-            UserProfile.objects.create(user=self, tier=TierFactory())
+            user_profile = UserProfile.objects.get(user=self)
+            user_profile.tier = TierFactory()
+            user_profile.save()
 
 
 class ProfileFactory(factory.django.DjangoModelFactory):
